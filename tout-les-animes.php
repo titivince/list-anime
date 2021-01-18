1fdd925873333830d66ca57ed224e9750bb0ce10
titivince <72084738+titivince@users.noreply.github.com>
2021-01-18 10:38:56 +0100

    Update fix require
    add link css

--- tout-les-animes.php
+++ tout-les-animes.php
@@ -9,15 +9,16 @@ $sql = $pdo->query("SELECT * FROM list ORDER BY id ");
 $animes = $sql->fetchAll();
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="asset/style.css">
     <title>Tout les animes</title>
 </head>
 <body>
     <?php require('src/nav.html'); ?>
-    <h1>Tout les animés</h1>
-    <?php require('main-block.php'); ?>
+    <h1 class="center">Tout les animés</h1>
+    <?php require('src/main-block.php'); ?>
 </body>
 </html>
