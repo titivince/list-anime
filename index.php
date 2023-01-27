a091df3b862269b2161652ba0f1d278ac70253bf
titivince <72084738+titivince@users.noreply.github.com>
2023-01-27 11:58:01 +0100

    adding mainClass from TNC folder
    adding a main class file to simplify the css later

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ $animes = $sql->fetchAll();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="asset/style.css">
+    <link rel="stylesheet" href="asset/mainClass.css">
     <title>Animes</title>
 </head>
 <body>
